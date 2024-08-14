d3d2a99332bb035008fa54b7f8f4c1c66a701893
akhi <ojamal@student.1337.ma>
2024-08-14 01:21:03 +0100

    chore: Refactor Dockerfile and Makefile for improved development workflow

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
+start:
+	docker-compose build && docker-compose up
+down:
+	docker-compose down
+restart:
+	docker-compose down && docker-compose up
+build:
+	docker-compose build
 up:
-	docker-compose build up
+	docker-compose up
+stop:
+	docker-compose stop
+rm:
+	docker-compose rm
+logs:
+	docker-compose logs
+ps:
+	docker-compose ps
--- srcs/script.js
+++ srcs/script.js
@@ -62,9 +62,59 @@ function update(){
 	ball.y += ball.dy;
 }
 
+function resetBall(){
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.dx = 0;
+    ball.dy = 0;
+}
+
+function ballMove(){
+    ball.dx += Math.random() < 0.5 ? -2 : 2;
+    ball.dy += Math.random() < 0.5 ? -2 : 2;
+}
+let score = {
+    right: 0,
+    left: 0
+};
+function ballWallCollision(){
+    if (ball.y + ball.rad > canvas.height || ball.y - ball.rad < 0) {
+        ball.dy = -ball.dy;
+    }
+    if (ball.x + ball.rad > canvas.width || ball.x - ball.rad < 0) {
+        if (ball.x + ball.rad > canvas.width) {
+            score.left++;
+        } else {
+            score.right++;
+        }
+        resetBall();
+    }
+}
+
+function ballPaddleCollision(){
+    if (ball.x - ball.rad < leftPaddle.x + leftPaddle.width && ball.y > leftPaddle.y && ball.y < leftPaddle.y + leftPaddle.height) {
+        ball.dx = -ball.dx + Math.random() % 10;
+    }
+    if (ball.x + ball.rad > rightPaddle.x && ball.y > rightPaddle.y && ball.y < rightPaddle.y + rightPaddle.height) {
+        ball.dx = -ball.dx + Math.random() % 10;
+    }
+}
+
+function scoreDisplay(){
+    game.font = '20px Arial';
+    game.fillText(score.left, canvas.width / 2 - 30, 30);
+    game.fillText(score.right, canvas.width / 2 + 10, 30);
+}
+
 function gameLoop(){
 	update();
 	draw();
+    scoreDisplay();
+    ballWallCollision();
+    ballPaddleCollision();
+    if (ball.dx === 0 && ball.dy === 0) {
+        ballMove();
+    }
 	requestAnimationFrame(gameLoop);
 }
 document.addEventListener('keydown', keyDownHandler);
@@ -72,16 +122,29 @@ document.addEventListener('keyup', keyUpHandler);
 
 function keyDownHandler(event) {
     if (event.key === 'ArrowUp') {
-        rightPaddle.dy = -10;
+        rightPaddle.dy = -5;
         if (rightPaddle.y + rightPaddle.dy < 0) {
             rightPaddle.y = 0;
             rightPaddle.dy = 0;
         }
     } else if (event.key === 'ArrowDown') {
-        rightPaddle.dy = 10;
+        rightPaddle.dy = 5;
         if (rightPaddle.y + rightPaddle.height + rightPaddle.dy > canvas.height) {
             rightPaddle.y = canvas.height - rightPaddle.height;
             rightPaddle.dy = 0;
+        }
+    }
+    if (event.key === 'w') {
+        leftPaddle.dy = -5;
+        if (leftPaddle.y + leftPaddle.dy < 0) {
+            leftPaddle.y = 0;
+            leftPaddle.dy = 0;
+        }
+    } else if (event.key === 's') {
+        leftPaddle.dy = 5;
+        if (leftPaddle.y + leftPaddle.height + leftPaddle.dy > canvas.height) {
+            leftPaddle.y = canvas.height - leftPaddle.height;
+            leftPaddle.dy = 0;
         }
     }
 }
@@ -91,6 +154,8 @@ function keyUpHandler(event)  
  {
     if (event.key === 'ArrowUp' || event.key === 'ArrowDown')
         rightPaddle.dy = 0;
+    if (event.key === 'w' || event.key === 's')
+        leftPaddle.dy = 0;
 }
 
 gameLoop();
