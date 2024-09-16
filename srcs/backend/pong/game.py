a89947368251456ee95f8166a036f7e76608b4c6
akhi <ojamal@student.1337.ma>
2024-09-16 16:33:08 +0100

    Refactor game.py and views.py in pong module

---
+++ srcs/backend/pong/game.py
@@ -0,0 +1,112 @@
+import random
+
+
+canvasWidth = 800
+canvasHeight = 600
+
+class Paddle:
+    def __init__(self, x, y, width=25, height=150, dy=5, color='#3498db'):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.dy = dy
+        self.ballTouchedPaddle = False
+        self.color = color
+
+class Ball:
+    def __init__(self, x, y, radius=15, dx=0, dy=0, speed=5):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.dx = dx
+        self.dy = dy
+        self.speed = speed
+
+left_paddle = Paddle(0, canvasHeight // 2 - 75)
+right_paddle = Paddle(canvasWidth - 25, canvasHeight // 2 - 75, color='#e74c3c')
+ball = Ball(canvasWidth // 2, canvasHeight // 2)
+
+score = {'left': 0, 'right': 0}
+
+class Game:
+    def __init__(self, canvasWidth, canvasHeight, left_paddle, right_paddle, ball, score):
+        self.canvasWidth = canvasWidth
+        self.canvasHeight = canvasHeight
+        self.left_paddle = left_paddle
+        self.right_paddle = right_paddle
+        self.ball = ball
+        self.score = score
+
+def reset_ball(ball):
+    ball.x = canvasWidth / 2
+    ball.y = canvasHeight / 2
+    ball.dx = 0
+    ball.dy = 0
+    ball.speed = 5
+
+def ball_move(ball):
+    if random.random() < 0.5:
+        ball.dx = 2.5
+        ball.dy = 2.5
+    else:
+        ball.dx = -2.5
+        ball.dy = -2.5
+
+def ball_wall_collision(ball):
+    global score
+    
+    if ball.y + ball.radius > canvasHeight or ball.y - ball.radius < 0:
+        ball.dy = -ball.dy
+    
+    if ball.x + ball.radius > canvasWidth or ball.x - ball.radius < 0:
+        if ball.x + ball.radius > canvasWidth:
+            score['left'] += 1
+        else:
+            score['right'] += 1
+        
+        reset_ball(ball)
+
+def ball_paddle_collision(ball, left_paddle, right_paddle):
+    if (ball.x - ball.radius < left_paddle.x + left_paddle.width and
+        ball.y + ball.radius > left_paddle.y and
+        ball.y - ball.radius < left_paddle.y + left_paddle.height and
+        ball.dx < 0):
+        
+        ball.dx = -ball.dx + (random.random() * 0.5)
+        left_paddle.ballTouchedPaddle = True
+    
+    elif (ball.x + ball.radius > right_paddle.x and
+          ball.y + ball.radius > right_paddle.y and
+          ball.y - ball.radius < right_paddle.y + right_paddle.height and
+          ball.dx > 0):
+        
+        ball.dx = -ball.dx - (random.random() * 0.5)
+        right_paddle.ballTouchedPaddle = True
+    
+    ball.speed += 0.1
+
+def paddle_move(direction):
+    if (direction == 'up'):
+        right_paddle.dy = -5
+    elif (direction == 'down'):
+        right_paddle.dy = 5
+
+def paddle_stop():
+    if (right_paddle.dy != 0):
+        right_paddle.dy = 0
+
+def paddleMovements(request):
+    if (request.GET.get('direction') == 'up'):
+        paddle_move('up')
+    elif (request.GET.get('direction') == 'down'):
+        paddle_move('down')
+    elif (request.GET.get('direction') == 'stop'):
+        paddle_stop()
+
+if (ball.dx == 0 and ball.dy == 0):
+	ball_move(ball)
+
+if (score['left'] == 11 or score['right'] == 11):
+	reset_ball(ball)
+	score = {'left': 0, 'right': 0}
--- srcs/backend/pong/views.py
+++ srcs/backend/pong/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
-from . import __init__
+from .game import Game
 
 def test(request):
     return HttpResponse("Backend is working!")
@@ -20,13 +20,13 @@ def home(request):
 #     }
 #     return Response(data)
 def game_state(request):
-    __init__.ball_move(__init__.ball)
-    __init__.ball_wall_collision(__init__.ball)
-    __init__.ball_paddle_collision(__init__.ball, __init__.left_paddle, __init__.right_paddle)
-    __init__.paddleMovements(request)
+    Game.ball_move(Game.ball)
+    Game.ball_wall_collision(Game.ball)
+    Game.ball_paddle_collision(Game.ball, Game.left_paddle, Game.right_paddle)
+    Game.paddleMovements(request)
     return JsonResponse({
-        'ball': {'x': __init__.ball.x, 'y': __init__.ball.y, 'radius': __init__.ball.radius},
-        'left_paddle': {'x': __init__.left_paddle.x, 'y': __init__.left_paddle.y, 'width': __init__.left_paddle.width, 'height': __init__.left_paddle.height},
-        'right_paddle': {'x': __init__.right_paddle.x, 'y': __init__.right_paddle.y, 'width': __init__.right_paddle.width, 'height': __init__.right_paddle.height},
-        'score': __init__.score
+        'ball': {'x': Game.ball.x, 'y': Game.ball.y, 'radius': Game.ball.radius},
+        'left_paddle': {'x': Game.left_paddle.x, 'y': Game.left_paddle.y, 'width': Game.left_paddle.width, 'height': Game.left_paddle.height},
+        'right_paddle': {'x': Game.right_paddle.x, 'y': Game.right_paddle.y, 'width': Game.right_paddle.width, 'height': Game.right_paddle.height},
+        'score': Game.score
     })
