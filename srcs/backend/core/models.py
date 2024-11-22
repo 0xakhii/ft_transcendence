583e390ec4a099edf5840f4faf56cb5ac97c96d4
Akhi <ojamal@student.1337.ma>
2024-11-22 02:11:05 +0100

    fix back/front migration issues

--- srcs/backend/core/models.py
+++ srcs/backend/core/models.py
@@ -13,10 +13,10 @@ import random
 
 def ball_move(ball):
 	print("ball_move")
-	ball.dx = random.choice([-1, 1])
-	ball.dy = random.choice([-1, 1])
+	# ball.dx = random.choice([-1, 1])
+	# ball.dy = random.choice([-1, 1])
 
 def init(left_paddle, right_paddle, ball):
 	print("init")
-	if (ball.dx == 0 and ball.dy == 0):
-		ball_move(ball)
+	# if (ball.dx == 0 and ball.dy == 0):
+	ball_move(ball)
