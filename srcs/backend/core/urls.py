a781ed6caab08c8b72b8791162e84cd270c4d6f3
Omar Jamal <ojamal@e2r11p2.1337.ma>
2024-11-22 02:01:10 +0100

    fix back/front migration issues

--- srcs/backend/core/urls.py
+++ srcs/backend/core/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views, const
 
 urlpatterns = [
-	path('win/', const.getWinSize, name='getWinSize'),
 	path('game/', views.game_state, name='game_state'),
 ]
--- srcs/backend/core/views.py
+++ srcs/backend/core/views.py
@@ -3,18 +3,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from core.models import init
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotAllowed
+import json
 
-@api_view(['GET'])
+@csrf_exempt
 def game_state(request):
-    game = init()
-    # game.ball_move(game.ball)
-    # game.ball_wall_collision(game.ball)
-    # game.ball_paddle_collision(game.ball, game.left_paddle, game.right_paddle)
-    # game.paddleMovements(request)
-    return JsonResponse({
-        'ball': {'x': game.ball.x, 'y': game.ball.y, 'radius': game.ball.radius},
-        'left_paddle': {'x': game.left_paddle.x, 'y': game.left_paddle.y, 'width': game.left_paddle.width, 'height': game.left_paddle.height},
-        'right_paddle': {'x': game.right_paddle.x, 'y': game.right_paddle.y, 'width': game.right_paddle.width, 'height': game.right_paddle.height},
-        'score': game.score,
-    })
-    
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        left_paddle = data.get('leftPaddle')
+        right_paddle = data.get('rightPaddle')
+        ball = data.get('ball')
+        init(left_paddle, right_paddle, ball)
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'}, status=400)
