2fe3f1d7850a6cfce18c043ed0d18cd77c30f4b8
Akhi <ojamal@student.1337.ma>
2024-11-22 02:06:48 +0100

    fix back/front migration issues

--- srcs/backend/core/models.py
+++ srcs/backend/core/models.py
@@ -1,7 +1,7 @@
 # from django.db import models
 
 # Create your models here.
-# import random
+import random
 
 
 # if (ball.dx == 0 and ball.dy == 0):
@@ -11,5 +11,12 @@
 # 	reset_ball(ball)
 # 	score = {'left': 0, 'right': 0}
 
+def ball_move(ball):
+	print("ball_move")
+	ball.dx = random.choice([-1, 1])
+	ball.dy = random.choice([-1, 1])
+
 def init(left_paddle, right_paddle, ball):
 	print("init")
+	if (ball.dx == 0 and ball.dy == 0):
+		ball_move(ball)
