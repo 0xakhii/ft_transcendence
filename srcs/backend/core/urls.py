1e144f8d883023e4170973685d29495e371e6701
Akhi <ojamal@student.1337.ma>
2024-11-23 19:48:59 +0100

    Revert "fix back/front migration issues"
    This reverts commit a781ed6caab08c8b72b8791162e84cd270c4d6f3.
    
    	modified:   srcs/backend/core/models.py
    	modified:   srcs/backend/core/urls.py
    	modified:   srcs/backend/core/views.py
    	modified:   srcs/game.js
    	modified:   srcs/index.html

--- srcs/backend/core/urls.py
+++ srcs/backend/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views, const
 
 urlpatterns = [
+	path('win/', const.getWinSize, name='getWinSize'),
 	path('game/', views.game_state, name='game_state'),
 ]
